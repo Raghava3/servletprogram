8ed1b1490b3520106518ea6eba92e5db990d09d4
raghav <1raghuram3@gmail.com>
2017-03-06 09:03:48 +0530

    updated

---
+++ All.java
@@ -0,0 +1,275 @@
+package con.ipl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import com.mysql.jdbc.Driver;
+import com.mysql.jdbc.Statement;
+
+
+public class All  extends HttpServlet
+{
+	Connection con = null;
+	PreparedStatement pstmt1 =null;
+	Statement stm=null;
+	ResultSet resultSet=null;
+	int count1=0;
+	private  Connection  jdbc()
+	{
+
+		try {
+
+
+			Driver dref= new Driver();
+			DriverManager.registerDriver(dref);
+
+			String dburl= ("jdbc:mysql://localhost:3306/Raghava?user=root&password=root");
+			con=DriverManager.getConnection(dburl);
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException 
+			{
+
+		String s=req.getRequestURI();
+		if(s.endsWith("register"))
+
+		{
+			String firstname=req.getParameter("firstname");
+			String middlename=req.getParameter("middlename");
+			String lastname=req.getParameter("lastname");
+			String email=req.getParameter("email");
+			String password=req.getParameter("pass");
+			resp.setContentType("text/html");
+			PrintWriter out = resp.getWriter();
+			Connection con=jdbc();
+
+			String query1 ="insert into iplteam_signup values(?,?,?,?,?)";
+			try 
+			{
+				pstmt1 = con.prepareStatement(query1);
+				pstmt1.setString(1,firstname);
+				pstmt1.setString(2,middlename);
+				pstmt1.setString(3,lastname);
+				pstmt1.setString(4,email);
+				pstmt1.setString(5,password);
+				count1 =pstmt1.executeUpdate();
+				out.print(count1);
+			}
+			catch (SQLException e) 
+			{
+				e.printStackTrace();
+			}
+
+			finally{
+
+				if (con != null) 
+				{
+					try
+					{
+						con.close();
+					}
+					catch(SQLException e)
+					{
+						e.printStackTrace();
+					}
+
+				}
+				if (pstmt1 != null) 
+				{
+					try
+					{
+						pstmt1.close();
+					}
+					catch(SQLException e)
+					{
+						e.printStackTrace();
+					}
+				}
+				if (resultSet != null) 
+				{
+					try
+					{
+						resultSet.close();
+					}
+					catch(SQLException e)
+					{
+						e.printStackTrace();
+					}
+				}
+			}
+			if(count1==1)
+			{
+
+				//System.out.println("com");
+			/*	out.print("	<html>");
+				out.print("<head>");
+				out.print("<title>");
+				out.print("JavaScript Automatic Page Redirect");
+				out.print("</title>");        	
+				out.print("	<script type='"+"text/javascript'>");
+				out.print("function pageRedirect()");
+				out.print("	    {");
+				out.print("   window.location.replace('"+"login.html'"+")");
+				out.print("    }  ");    
+				out.print("setTimeout('"+"pageRedirect()'"+")");
+				out.print("		</script>");
+				out.print("</head>");
+				out.print("</html>");*/
+				resp.sendRedirect("login.html");
+
+			}
+			else
+			{
+
+				//System.out.println("com");
+				/*out.print("	<html>");
+				out.print("<head>");
+				out.print("<title>");
+				out.print("JavaScript Automatic Page Redirect");
+				out.print("</title>");        	
+				out.print("	<script type='"+"text/javascript'>");
+				//		out.print("function pageRedirect()");
+				out.print("	    {");
+				out.print("   window.location.replace('"+"register.html'"+")");
+				out.print("    }  ");    
+				//		out.print("setTimeout('"+"pageRedirect()'"+")");
+				out.print("		</script>");
+				out.print("email alredy in use");
+				out.print("</head>");
+				out.print("</html>");
+*/
+				resp.sendRedirect("register.html");
+			}
+		}
+
+
+		else if(s.endsWith("form" ))
+		{
+
+
+			String mail=req.getParameter("email");
+			String password=req.getParameter("pass");
+
+			resp.setContentType("text/html");
+			PrintWriter out = resp.getWriter();
+
+			Connection con=jdbc();
+			String query2 ="select firstname from iplteam_signup where email like ? and password like ?";
+			try {
+
+				pstmt1 = con.prepareStatement(query2);
+
+				pstmt1.setString(1,mail);
+				pstmt1.setString(2,password);
+
+				resultSet =pstmt1.executeQuery();
+
+
+
+				if (resultSet.next())
+
+				{
+					/*out.print("	<html>");
+					out.print("<head>");
+					out.print("<title>");
+					out.print("JavaScript Automatic Page Redirect");
+					out.print("</title>");        	
+					out.print("	<script type='"+"text/javascript'>");
+					out.print("function pageRedirect()");
+					out.print("	    {");
+					out.print("   window.location.replace('"+"first.html'"+")");
+					out.print("    }  ");    
+					out.print("setTimeout('"+"pageRedirect()'"+")");
+					out.print("		</script>");
+					out.print("</head>");
+					out.print("</html>");*/
+					resp.sendRedirect("first.html");
+
+				}
+
+				else 
+				{
+					//	System.out.println(" email and password is not matching ");
+
+					/*out.print("	<html>");
+					out.print("<head>");
+					out.print("<title>");
+					out.print("JavaScript Automatic Page Redirect");
+					out.print("</title>");        	
+					out.print("	<script type='"+"text/javascript'>");
+					out.print("function pageRedirect()");
+					out.print("	    {");
+					out.print("   window.location.replace('"+"login.html'"+")");
+					out.print("    }  ");    
+					out.print("setTimeout('"+"pageRedirect()'"+")");
+					out.print("email and password is not matching");
+					out.print("		</script>");
+					out.print("</head>");
+					out.print("</html>");*/
+					resp.sendRedirect("login.html");
+
+				}
+			}
+			catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			finally{
+
+				if (con != null) 
+				{
+					try
+					{
+						con.close();
+					}
+					catch(SQLException e)
+					{
+						e.printStackTrace();
+					}
+
+				}
+				if (pstmt1 != null) 
+				{
+					try
+					{
+						pstmt1.close();
+					}
+					catch(SQLException e)
+					{
+						e.printStackTrace();
+					}
+					if (resultSet != null) 
+					{
+						try
+						{
+							resultSet.close();
+						}
+						catch(SQLException e)
+						{
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		}
+			}
+}
+
